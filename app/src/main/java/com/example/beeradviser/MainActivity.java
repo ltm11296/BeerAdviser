9a7b1b0fcfd46c6573a964b9f50e426e82709a86
Le Minh <lethanhminh11296@gmail.com>
2019-06-30 16:42:03 +0700

    initial commit

--- app/src/main/java/com/example/beeradviser/MainActivity.java
+++ app/src/main/java/com/example/beeradviser/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.beeradviser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickFindBeer(View view) {
+        com.example.beerexpert.Beerexpert expert = new com.example.beerexpert.Beerexpert();
+        TextView brands = findViewById(R.id.tv);
+        Spinner color = findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList) {
+            brandsFormatted.append(brand);
+        }
+        //Display the beers
+        brands.setText(brandsFormatted);
+
+    }
+
 }
